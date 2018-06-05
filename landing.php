63cfd69c581bc1a42c0816ef3a1e0aa25bf5ce55
Noah Herrin <noahherrin@gmail.com>
2018-06-05 09:28:13 -0700

    First Commit

---
+++ landing.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Landing</title>
+  </head>
+  <body>
+    <form class="login" action="php/home.php" method="post" target="_self">
+      <input type="text" name="username" value="username">
+      <input type="password" name="password" value="password">
+      <input type="submit" value="login">
+    </form>
+  </body>
+</html>
---
+++ php/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+  </head>
+  <body>
+    <?php echo $_POST["username"] </br>; ?>
+    <?php echo $_POST["password"] </br>; ?>
+
+  </body>
+</html>
